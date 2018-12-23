cff807bb61a7865cd05140674f698df966c04355
Takehiko Shinkura <tynmarket@gmail.com>
2018-12-23 18:28:37 +0900

    認証付加

--- spec/requests/api/stock_prices_spec.rb
+++ spec/requests/api/stock_prices_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 describe 'GET /api/stock_prices/:code' do
+  let(:code) { "1111" }
+  let(:date) { "2018-10-10" }
+
+  context "Authorizationヘッダなし" do
+    it "401" do
+      get api_stock_price_path(code), params: {date: date}
+
+      expect(response.status).to eq 401
+    end
+  end
+
+  context "tokenが不正" do
+    it "401" do
+      get api_stock_price_path(code), params: {date: date}, headers: {Authorization: "token bad_api_key"}
+
+      expect(response.status).to eq 401
+    end
+  end
+
   context 'date=2018-10-10' do
-    let(:code) { "1111" }
-    let(:date) { "2018-10-10" }
     let(:close) { 2100 }
     let(:change) { 50 }
     let(:change_rate) { 0.0233 }
@@ -17,7 +34,7 @@ describe 'GET /api/stock_prices/:code' do
     end
 
     it "" do
-      get api_stock_price_path(code), params: {date: date}
+      get api_stock_price_path(code), params: {date: date}, headers: auth_header
 
       expect(json[:close]).to eq close
       expect(json[:change]).to eq change
--- spec/support.rb
+++ spec/support.rb
@@ -2,4 +2,8 @@ module Support
   def json
     @json ||= JSON.parse(response.body).with_indifferent_access
   end
+
+  def auth_header
+    {Authorization: "token api_key"}
+  end
 end
