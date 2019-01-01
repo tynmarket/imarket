b8be1b9b3386a4dcea127815cba628ec21341ce8
Takehiko Shinkura <tynmarket@gmail.com>
2019-01-01 09:43:46 +0900

    Update build

---
+++ docker/mackerel-agent.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+LOGFILE=/var/log/mackerel-agent.log
+PIDFILE=/var/run/mackerel-agent.pid
+
+cd mackerel-agent
+echo 'roles = [ "iMarket:app" ]' >> mackerel-agent.conf
+echo "apikey = \"$MACKEREL_API_KEY\"" >> mackerel-agent.conf
+./mackerel-agent supervise -conf mackerel-agent.conf --pidfile=$PIDFILE >>$LOGFILE 2>&1 &
