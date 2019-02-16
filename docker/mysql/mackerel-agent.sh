eb61997b6b084a879a57263917fb04173e7b91b1
Takehiko Shinkura <tynmarket@gmail.com>
2019-02-16 17:14:23 +0900

    DISABLE_MACKEREL追加

--- docker/mysql/mackerel-agent.sh
+++ docker/mysql/mackerel-agent.sh
@@ -5,6 +5,10 @@ PIDFILE=/mackerel-agent/mackerel-agent.pid
 ROOT=/mackerel-agent
 
 (
+  if [ "${DISABLE_MACKEREL}" = "true" ]; then
+    exit 0
+  fi
+
   cd mackerel-agent
   echo 'roles = [ "iMarket:db" ]' >> mackerel-agent.conf
   echo "apikey = \"$MACKEREL_API_KEY\"" >> mackerel-agent.conf
