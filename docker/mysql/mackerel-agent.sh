52c01c0009187834ea0c0f6a2543020ca71d9a17
Takehiko Shinkura <tynmarket@gmail.com>
2019-02-16 15:23:06 +0900

    Mackerelを初期化スクリプトで起動

--- docker/mysql/mackerel-agent.sh
+++ docker/mysql/mackerel-agent.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-LOGFILE=/var/log/mackerel-agent.log
-PIDFILE=/var/run/mackerel-agent.pid
+LOGFILE=/mackerel-agent/mackerel-agent.log
+PIDFILE=/mackerel-agent/mackerel-agent.pid
+ROOT=/mackerel-agent
 
-cd mackerel-agent
-echo 'roles = [ "iMarket:db" ]' >> mackerel-agent.conf
-echo "apikey = \"$MACKEREL_API_KEY\"" >> mackerel-agent.conf
-./mackerel-agent supervise -conf mackerel-agent.conf --pidfile=$PIDFILE >>$LOGFILE 2>&1 &
+(
+  cd mackerel-agent
+  echo 'roles = [ "iMarket:db" ]' >> mackerel-agent.conf
+  echo "apikey = \"$MACKEREL_API_KEY\"" >> mackerel-agent.conf
+  ./mackerel-agent supervise -conf mackerel-agent.conf --pidfile=$PIDFILE --root=$ROOT >>$LOGFILE 2>&1 &
+)
