f6b9512d1f1dc0ccb683b834fc26352ea90a940a
Takehiko Shinkura <tynmarket@gmail.com>
2019-05-03 20:59:09 +0900

    キャッシュフローのNOT NULL制約を削除

--- db/migrate/20190426104314_create_cash_flows.rb
+++ db/migrate/20190426104314_create_cash_flows.rb
@@ -7,11 +7,11 @@ class CreateCashFlows < ActiveRecord::Migration[5.2]
       t.integer :year, limit: 2, null: false
       t.integer :month, limit: 1, null: false
       t.integer :quarter, limit: 1, null: false
-      t.integer :net_cash_provided_by_used_in_operating_activities, null: false
-      t.integer :net_cash_provided_by_used_in_investment_activities, null: false
-      t.integer :net_cash_provided_by_used_in_financing_activities, null: false
-      t.integer :net_increase_decrease_in_cash_and_cash_equivalents # 短信から取得した場合、値がない
-      t.integer :cash_and_cash_equivalents, null: false
+      t.integer :net_cash_provided_by_used_in_operating_activities # キャッシュフローの値が歯抜けの可能性もある
+      t.integer :net_cash_provided_by_used_in_investment_activities
+      t.integer :net_cash_provided_by_used_in_financing_activities
+      t.integer :net_increase_decrease_in_cash_and_cash_equivalents # 短信から取得した場合、値は設定されない
+      t.integer :cash_and_cash_equivalents
 
       t.timestamps
 
--- db/schema.rb
+++ db/schema.rb
@@ -19,11 +19,11 @@ ActiveRecord::Schema.define(version: 2019_04_26_104314) do
     t.integer "year", limit: 2, null: false
     t.integer "month", limit: 1, null: false
     t.integer "quarter", limit: 1, null: false
-    t.integer "net_cash_provided_by_used_in_operating_activities", null: false
-    t.integer "net_cash_provided_by_used_in_investment_activities", null: false
-    t.integer "net_cash_provided_by_used_in_financing_activities", null: false
+    t.integer "net_cash_provided_by_used_in_operating_activities"
+    t.integer "net_cash_provided_by_used_in_investment_activities"
+    t.integer "net_cash_provided_by_used_in_financing_activities"
     t.integer "net_increase_decrease_in_cash_and_cash_equivalents"
-    t.integer "cash_and_cash_equivalents", null: false
+    t.integer "cash_and_cash_equivalents"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_cash_flows_on_code"
