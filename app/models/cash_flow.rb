7a7cc719c6eae561e8d105f32da16fba90fcfdca
Takehiko Shinkura <tynmarket@gmail.com>
2019-05-04 10:54:17 +0900

    通期のみの場合は現金及び同等物の期末残高の差分を計算する

--- app/models/cash_flow.rb
+++ app/models/cash_flow.rb
@@ -7,4 +7,15 @@ class CashFlow < ApplicationRecord
   alias_attribute :net_increase_in_cash, :net_increase_decrease_in_cash_and_cash_equivalents
   alias_attribute :prior_cash, :prior_cash_and_cash_equivalents
   alias_attribute :cash, :cash_and_cash_equivalents
+
+  def net_increase_in_cash(prev_cash_flow = nil)
+    return net_increase_decrease_in_cash_and_cash_equivalents unless prev_cash_flow
+
+    if quarter == 4 && prev_cash_flow.quarter == 4
+      # 通期のみの場合は現金及び同等物の期末残高の差分を計算する
+      cash - prev_cash_flow.cash
+    else
+      net_increase_decrease_in_cash_and_cash_equivalents
+    end
+  end
 end
--- db/fixtures/cash_flows.rb
+++ db/fixtures/cash_flows.rb
@@ -1,4 +1,6 @@
 CashFlow.seed(:id,
   {id: 1, disclosure_id: 1, stock_id: 1, code: "1111", year: 2019, month: 3, quarter: 2, net_cash_provided_by_used_in_operating_activities: 3000, net_cash_provided_by_used_in_investment_activities: 3100, net_cash_provided_by_used_in_financing_activities: 3200, net_increase_decrease_in_cash_and_cash_equivalents: 9300, cash_and_cash_equivalents: 10300},
   {id: 2, disclosure_id: 5, stock_id: 1, code: "1111", year: 2019, month: 3, quarter: 4, net_cash_provided_by_used_in_operating_activities: 4000, net_cash_provided_by_used_in_investment_activities: 5100, net_cash_provided_by_used_in_financing_activities: 6200, net_increase_decrease_in_cash_and_cash_equivalents: 15300, cash_and_cash_equivalents: 16300},
+  {id: 3, disclosure_id: 3, stock_id: 2, code: "2222", year: 2018, month: 3, quarter: 4, net_cash_provided_by_used_in_operating_activities: 3000, net_cash_provided_by_used_in_investment_activities: 3100, net_cash_provided_by_used_in_financing_activities: 3200, cash_and_cash_equivalents: 10300},
+  {id: 4, disclosure_id: 4, stock_id: 2, code: "2222", year: 2019, month: 3, quarter: 4, net_cash_provided_by_used_in_operating_activities: 4000, net_cash_provided_by_used_in_investment_activities: 5100, net_cash_provided_by_used_in_financing_activities: 6200, cash_and_cash_equivalents: 16300},
 )
