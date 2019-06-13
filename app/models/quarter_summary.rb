bda416cf3be3b5763006e61aa882f0dff699a5f0
Takehiko Shinkura <tynmarket@gmail.com>
2019-06-13 20:26:25 +0900

    Layout/SpaceAroundOperators適用

--- app/models/quarter_summary.rb
+++ app/models/quarter_summary.rb
@@ -39,7 +39,7 @@ class QuarterSummary
       elsif quarter == 0
         @summary.send(method)
       elsif @prev_quarter_summary &&  # 決算期の変更
-            (year != @prev_quarter_summary.year ||  # 年度の途中から年度が変わっている
+            (year != @prev_quarter_summary.year || # 年度の途中から年度が変わっている
               (year == @prev_quarter_summary.year && month != @prev_quarter_summary.month))  # 年度の途中から決算月が変わっている
         @summary.send(method)
       elsif @summary.send(method) && @prev_quarter_summary.try(method)
