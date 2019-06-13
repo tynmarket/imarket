918984a509372b6156ff8d48f640ef113730edd2
Takehiko Shinkura <tynmarket@gmail.com>
2019-06-13 20:56:38 +0900

    Style/ClassAndModuleChildren適用

--- app/controllers/api/stock_prices_controller.rb
+++ app/controllers/api/stock_prices_controller.rb
@@ -1,14 +1,16 @@
-class Api::StockPricesController < Api::ApplicationController
-  def show
-    code = params[:code]
-    date = params[:date]
-    @stock_price = StockPrice
-                   .joins(:stock)
-                   .daily
-                   .where(stocks: { code: code })
-                   .where(date: date)
-                   .first
+class Api
+  class StockPricesController < Api::ApplicationController
+    def show
+      code = params[:code]
+      date = params[:date]
+      @stock_price = StockPrice
+                     .joins(:stock)
+                     .daily
+                     .where(stocks: { code: code })
+                     .where(date: date)
+                     .first
 
-    render json: (@stock_price || {})
+      render json: (@stock_price || {})
+    end
   end
 end
