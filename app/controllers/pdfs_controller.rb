2643c37ab961c6dbb8a56daf55b839cc4bd1d770
Takehiko Shinkura <tynmarket@gmail.com>
2019-07-18 08:06:38 +0900

    WIP

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -2,7 +2,10 @@ class PdfsController < ApplicationController
   layout "pdf"
 
   def show
+    @path = params[:path]
+
     respond_to do |format|
+      format.pdf { render file: "#{Rails.root}/public/404.html" }
       format.any
     end
   end
