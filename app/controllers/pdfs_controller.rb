03fc261a2fedea1ec8ad9886c3d60066aef8ab99
Takehiko Shinkura <tynmarket@gmail.com>
2019-07-20 18:41:43 +0900

    ファイルパス修正

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -2,9 +2,10 @@ class PdfsController < ApplicationController
   layout "pdf"
 
   def show
-    @path = params[:path]
-    pdf = @path&.split("/")&.last
-    @code = Disclosure.find_by(pdf: pdf)&.code
+    path = params[:path]
+    pdf = path&.split("/")&.last
+    @pdf = "#{pdf}.pdf"
+    @code = Disclosure.find_by(pdf: @pdf)&.code
   end
 
 end
