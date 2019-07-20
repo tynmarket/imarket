c7ef6bff33a5c6e819fce2e956ed1fa676680a7d
Takehiko Shinkura <tynmarket@gmail.com>
2019-07-20 18:02:06 +0900

    Fix

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -5,11 +5,6 @@ class PdfsController < ApplicationController
     @path = params[:path]
     pdf = @path&.split("/")&.last
     @code = Disclosure.find_by(pdf: pdf)&.code
-
-    respond_to do |format|
-      format.pdf { render file: "#{Rails.root}/public/404.html" }
-      format.any
-    end
   end
 
 end
