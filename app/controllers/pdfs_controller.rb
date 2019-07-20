6b3636d19d231284d3a35d30692dcafd7a93e396
Takehiko Shinkura <tynmarket@gmail.com>
2019-07-20 17:05:36 +0900

    PC画面に個別銘柄のページのリンクを追加

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -3,6 +3,8 @@ class PdfsController < ApplicationController
 
   def show
     @path = params[:path]
+    pdf = @path&.split("/")&.last
+    @code = Disclosure.find_by(pdf: pdf)&.code
 
     respond_to do |format|
       format.pdf { render file: "#{Rails.root}/public/404.html" }
