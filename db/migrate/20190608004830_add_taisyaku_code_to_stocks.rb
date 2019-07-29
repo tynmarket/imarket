862c8d22ddd02bc28a3c470ebcba44b2bfb3f054
Takehiko Shinkura <tynmarket@gmail.com>
2019-07-29 22:39:00 +0900

    名前変更

--- db/migrate/20190608004830_add_taisyaku_code_to_stocks.rb
+++ db/migrate/20190608004830_add_taisyaku_code_to_stocks.rb
@@ -1,4 +1,4 @@
-class Add < ActiveRecord::Migration[5.2]
+class AddTaisyakuCodeToStocks < ActiveRecord::Migration[5.2]
   def change
     add_column :stocks, :taisyaku_code, :string, limit: 6, after: :is_consolidated
   end
