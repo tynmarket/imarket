abfa4740ea102cb27a4ae94fcdd54f5c4a61f768
Takehiko Shinkura <tynmarket@gmail.com>
2019-08-05 22:05:50 +0900

    ログインのルーティング

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def destroy
     logout
     redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "disclosures#index"
 
   # 認証
+  get "login", to: "sessions#new"
   post "logout", to: "sessions#destroy"
   get "oauth/callback" => "oauths#callback"
   get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
