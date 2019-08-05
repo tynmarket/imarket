d0603b64fa15898875dcd2656f84675d1943f762
Takehiko Shinkura <tynmarket@gmail.com>
2019-08-05 22:05:16 +0900

    ログアウト

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "disclosures#index"
 
   # 認証
+  post "logout", to: "sessions#destroy"
   get "oauth/callback" => "oauths#callback"
   get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
 
