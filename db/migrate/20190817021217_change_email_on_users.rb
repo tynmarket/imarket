40498fba1427842e6fb4ec458172d9b061c498a4
Takehiko Shinkura <tynmarket@gmail.com>
2019-08-17 11:18:24 +0900

    emailをNULL可、name追加

---
+++ db/migrate/20190817021217_change_email_on_users.rb
@@ -0,0 +1,9 @@
+class ChangeEmailOnUsers < ActiveRecord::Migration[6.0]
+  def up
+    change_column :users, :email, :string, null: true
+  end
+
+  def down
+    change_column :users, :email, :string, null: false
+  end
+end
