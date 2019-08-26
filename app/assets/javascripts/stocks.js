2967c9b945c0d7fefc7cbf293a1f517ac8aa3b29
Takehiko Shinkura <tynmarket@gmail.com>
2019-08-26 19:49:33 +0900

    時価総額/FCF倍率のタブを追加

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -11,6 +11,8 @@ $(function () {
   trackEvent('#tab-per-track', 'per', label);
   // PBR
   trackEvent('#tab-pbr-track', 'pbr', label);
+  // 時価総額/FCF倍率
+  trackEvent('#tab-market-cap-fcf-ratio-track', 'market-cap-fcf-ratio', label);
   // 全て
   trackEvent('#tab-disclosures', 'disclosures', label);
   // チャート
