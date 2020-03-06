1133dd35f5e860dd1d1cbae00e40c6f0eb02afa8
Takehiko Shinkura <tynmarket@gmail.com>
2020-03-06 20:50:29 +0900

    不要コードを削除

--- app/models/system_status.rb
+++ app/models/system_status.rb
@@ -3,13 +3,13 @@ class SystemStatus < ActiveRecord::Base
   # 株価反映日時
   STOCK_PRICE_LAST_UPDATED = 1
 
-  def self.stock_price_last_updated(format = true)
+  def self.stock_price_last_updated
     system_status = find_by id: STOCK_PRICE_LAST_UPDATED
 
     return unless system_status
 
     last_updated = DateTime.parse system_status.status
-    format ? last_updated.strftime(Utils::Constants::YMD_HM_KA_LA) : last_updated
+    last_updated.strftime(Utils::Constants::YMD_HM_KA_LA)
   end
 
 end
--- spec/models/system_status_spec.rb
+++ spec/models/system_status_spec.rb
@@ -3,26 +3,15 @@ require "rails_helper"
 describe SystemStatus do
 
   describe ".stock_price_last_updated" do
-    let(:format) { true }
-    let(:last_updated) { SystemStatus.stock_price_last_updated format }
+    let(:last_updated) { SystemStatus.stock_price_last_updated }
 
     context "データあり" do
       let(:status) { "2014年1月1日 0時0分" }
 
-      before do
-        create :system_status, :stock_price_last_updated, status: "2014-01-01T00:00:00+09:00"
-      end
+      before { create :system_status, :stock_price_last_updated, status: "2014-01-01T00:00:00+09:00" }
 
       it "株価反映日時を取得する" do
         expect(last_updated).to eq status
-      end
-
-      context "format = false" do
-        let(:format) { false }
-
-        it "DateTimeで取得する" do
-          expect(last_updated).to eq DateTime.new(2014, 1, 1, 0, 0, 0, "+0900")
-        end
       end
     end
 
@@ -32,5 +21,4 @@ describe SystemStatus do
       end
     end
   end
-
 end
