e379bf598a647c23cd34ea0289a36dd9b9f307c6
Takehiko Shinkura <tynmarket@gmail.com>
2020-04-19 11:31:46 +0900

    Fix

--- app/models/eps_estimate.rb
+++ app/models/eps_estimate.rb
@@ -4,7 +4,8 @@ class EpsEstimate < ApplicationRecord
 
   class << self
     def codes_dow
-      %w(AAPL AXP BA CAT CSCO CVX DIS DOW GS HD IBM INTC JNJ JPM KO MCD MMM MRK MSFT NKE PFE PG RTX TRV UNH V VZ WBA WMT XOM)
+      %w(AAPL AXP BA CAT CSCO CVX DIS DOW GS HD IBM INTC JNJ JPM KO
+         MCD MMM MRK MSFT NKE PFE PG RTX TRV UNH V VZ WBA WMT XOM)
     end
   end
 end
