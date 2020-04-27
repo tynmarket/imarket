604342c4542ad879d8436cfcc90f2d1aef6ad211
Takehiko Shinkura <tynmarket@gmail.com>
2020-04-27 21:11:01 +0900

    株式分割を表示

--- app/controllers/stock_splits_controller.rb
+++ app/controllers/stock_splits_controller.rb
@@ -1,9 +1,16 @@
 class StockSplitsController < ApplicationController
   def index
-    codes = RevisionHistory.includes(constituent_stocks: :stock).last.constituent_stocks.map(&:code)
+    codes = RevisionHistory
+            .includes(constituent_stocks: :stock)
+            .last
+            .constituent_stocks
+            .map(&:code)
+
     @stock_splits = StockSplit
                     .includes(:stock)
-                    .where(code: codes)
                     .order("id desc")
+
+    @stock_splits_n225 = @stock_splits
+                         .select { |stock_split| codes.include?(stock_split.code) }
   end
 end
