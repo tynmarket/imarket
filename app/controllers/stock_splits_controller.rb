65ffc7eedbf23749fbc0944ee38d1aff77da945a
Takehiko Shinkura <tynmarket@gmail.com>
2020-04-27 21:26:48 +0900

    最大2年分表示

--- app/controllers/stock_splits_controller.rb
+++ app/controllers/stock_splits_controller.rb
@@ -1,5 +1,6 @@
 class StockSplitsController < ApplicationController
   def index
+    from_date = Date.today.beginning_of_year - 1.years
     codes = RevisionHistory
             .includes(constituent_stocks: :stock)
             .last
@@ -8,6 +9,7 @@ class StockSplitsController < ApplicationController
 
     @stock_splits = StockSplit
                     .includes(:stock)
+                    .where("split_date > ?", from_date)
                     .order("id desc")
 
     @stock_splits_n225 = @stock_splits
