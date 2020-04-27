e5582e33a2f305ee974b9d0c64f0cd80fe7893c4
Takehiko Shinkura <tynmarket@gmail.com>
2020-04-27 22:04:32 +0900

    Fix

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -21,18 +21,20 @@ module MetaTagsHelper
   end
 
   def set_title_and_description_others
-    title =
-      if controller_name == "favorites"
-        "お気に入り | "
-      elsif @stock
-        "【#{@stock.code}】#{@stock.name}の決算短信 | "
-      elsif @date && !current_page?(root_path)
-        "#{l(@date, format: :ymd_k)}の決算短信 | "
-      end
     head = @stock.name || l(@date, format: :ymd_k) || "東証"
 
-    @title = "#{title}iMarket（適時開示ネット）"
+    @title = "#{meta_title_others}iMarket（適時開示ネット）"
     @description = "#{head}の適時開示で開示された決算短信や業績予想の修正の一覧が見られます。" +
       "四半期ごとの業績の推移や前年比、営業利益率、予想PERとPBRの時系列データをグラフ確認できます。"
+  end
+
+  def meta_title_others
+    if controller_name == "favorites"
+      "お気に入り | "
+    elsif @stock
+      "【#{@stock.code}】#{@stock.name}の決算短信 | "
+    elsif @date && !current_page?(root_path)
+      "#{l(@date, format: :ymd_k)}の決算短信 | "
+    end
   end
 end
