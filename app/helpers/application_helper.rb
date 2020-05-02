cb76dd0188a85d214bb9a25a625bd4d9784ad8f2
Takehiko Shinkura <tynmarket@gmail.com>
2020-05-02 10:10:23 +0900

    リンク修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
 
   def release_notes
     <<~EOS.html_safe
-      （4/28）<a href="/eps_estimates">日経平均</a> と <a href="/eps_estimates">ダウ平均</a> の予想EPSを表示するようにしました。<br>
+      （4/28）<a href="/eps_estimates">日経平均</a> と <a href="/eps_estimates?tab=dow">ダウ平均</a> の予想EPSを表示するようにしました。<br>
        <span style="color:white;">（4/28）</span>日経平均は公式のものとiMarket算出の2種類を載せています。
     EOS
   end
